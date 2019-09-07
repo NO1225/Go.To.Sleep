1b67b01e2939035ee51ef00d010f062082b2acd1
safartyjumah <safartyjumah@gmail.com>
2019-09-07 22:37:29 +0300

    Commenting
    Commenting the code behind file of the main window

--- Go.To.Sleep/Go.To.Sleep/MainWindow.xaml.cs
+++ Go.To.Sleep/Go.To.Sleep/MainWindow.xaml.cs
@@ -24,14 +24,23 @@ namespace Go.To.Sleep
     {
         #region Private Fields
 
-        private Point _mouseOffsetToCenter;
+        /// <summary>
+        /// The stating position of the mouse when the drag drop started
+        /// </summary>
+        private Point _mouseStartingPosition;
 
+        /// <summary>
+        /// The starting angle of the handle when the drag drop started
+        /// </summary>
         private double _startingAngle;
 
         #endregion
 
         #region Constructor
 
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
@@ -44,31 +53,45 @@ namespace Go.To.Sleep
         
         #region Events
 
+        /// <summary>
+        /// Event to hundle the begining of the drag drop, to assign the values which should be assigned at the start of the drag drop
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Thumb_DragStarted(object sender, DragStartedEventArgs e)
         {
-            var h = e.HorizontalOffset;
-            var v = e.VerticalOffset;
-
-            _mouseOffsetToCenter = new Point(
-                150 - h,
-                150 - v
+            // Get the mouse position relative to the center of the handel
+            _mouseStartingPosition = new Point(
+                150 - e.HorizontalOffset,
+                150 - e.VerticalOffset
                 );
+
+            // Get the angle of the handle
             _startingAngle = ((ApplicationViewModel)this.DataContext).Angle;
         }
 
+
+        /// <summary>
+        /// Event to handle the dragging, and updating the angle in respect to the angle of the mouse relative to the starting position of the dragging
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Thumb_DragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
         {
+            // Get the new position of the mouse 
             Point newPosition = new Point(
-                _mouseOffsetToCenter.X - e.HorizontalChange,
-                _mouseOffsetToCenter.Y - e.VerticalChange
+                _mouseStartingPosition.X - e.HorizontalChange,
+                _mouseStartingPosition.Y - e.VerticalChange
                 );
 
-            double angle1 = ArcTan(_mouseOffsetToCenter.X, _mouseOffsetToCenter.Y);
+            // Computing the angles
+            double angle1 = ArcTan(_mouseStartingPosition.X, _mouseStartingPosition.Y);
 
             double angle2 = ArcTan(newPosition.X, newPosition.Y);
 
             var angle = (angle2 - angle1);
 
+            // Update the angle
             ((ApplicationViewModel)this.DataContext).Angle = _startingAngle + angle;
         }
 
@@ -77,9 +100,18 @@ namespace Go.To.Sleep
 
         #region Helping Methods
 
+        /// <summary>
+        /// Computing the arc tan and converting it as required in respect to the quarter of the angle in the cartizian system
+        /// </summary>
+        /// <param name="x">The x coordination of the vector</param>
+        /// <param name="y">The y coordination of the vector</param>
+        /// <returns>The computed angle in degrees</returns>
         public double ArcTan(double x, double y)
         {
+            // Get the angle and convert it to degrees
             double angle = Math.Atan(y / x) * 180 / Math.PI;
+
+            // Farther processing according to where the angle reside
             if (x >= 0 && y >= 0)
                 return angle;
             else if (x >= 0 && y < 0)
